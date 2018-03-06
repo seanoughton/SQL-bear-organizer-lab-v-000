29cc1920f5fae1aef503f1da9d277de67b2eca1d
Sean Oughton <seanoughton@gmail.com>
2018-03-06 00:00:17 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   #count the color , returns that color with how many of it
-  "SELECT color, COUNT(color) FROM bears"
+  "SELECT color, COUNT(color) FROM bears WHERE color = 'dark brown' "
 end
 
 def counts_number_of_bears_with_goofy_temperaments
